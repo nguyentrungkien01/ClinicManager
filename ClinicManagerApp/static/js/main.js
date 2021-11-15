704fbd9547426d93f22fc6559038da4319850cec
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-11-15 22:01:39 +0700

    base fe

---
+++ ClinicManagerApp/static/js/main.js
@@ -0,0 +1,34 @@
+$(function () {
+    'use strict';
+    //===== Prealoder
+    window.addEventListener("load", () => {
+        document.body.classList.add("loaded");
+    });
+
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200)
+        } else {
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+
+})
+
+// init cursor
+var cursors = [{
+    cursor_id: "3",
+    cursor_type: "0",
+    cursor_shape: "15",
+    cursor_image: "",
+    default_cursor: "auto",
+    hover_effect: "plugin",
+    body_activation: "1",
+    element_activation: "0",
+    selector_type: "tag",
+    selector_data: "body",
+    color: "#cc3a3b",
+    width: "30",
+    blending_mode: "normal"
+}];
