553a7679ee07aa7d8df2f0be16562fa57a9460b6
= <=>
2021-12-15 16:23:25 +0700

    test pull

--- ClinicManagerApp/main.py
+++ ClinicManagerApp/main.py
@@ -1,10 +1,12 @@
+from flask import render_template
+
 from ClinicManagerApp import initTables, initAdmin
 from ClinicManagerApp.controller.admin.account_controller import *
 
 
 @app.route('/')
 def homePage():
-    return "Hello"
+    return render_template('/client/index.html')
 
 
 if __name__ == '__main__':
