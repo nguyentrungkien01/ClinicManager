3817701e213b31da77f3effd85d7169c636b6922
namnguyenthanhwork <78526193+namnguyenthanhwork@users.noreply.github.com>
2021-12-16 08:47:02 +0700

    fix init py

--- ClinicManagerApp/main.py
+++ ClinicManagerApp/main.py
@@ -1,11 +1,5 @@
 from flask import render_template
-
-<<<<<<< HEAD
-from ClinicManagerApp import initTables, initAdmin
-from ClinicManagerApp.controller.admin.account_controller import *
-=======
 from ClinicManagerApp import initTables, initAdmin, app
->>>>>>> 53c8531ff497b574d8c55c2b90e7f166a00a33b0
 
 
 @app.route('/')
